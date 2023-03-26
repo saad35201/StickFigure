61b4d0e55b913e783a15ff67c93d042613fff7b5
saad35201 <saadasad35201@gmail.com>
2023-03-26 03:37:14 -0700

    forgot and reset password screen design complete

--- app/src/main/java/com/saadi/stickfigure/feature_auth/presentation/forgot_password/FragmentForgotPassword.kt
+++ app/src/main/java/com/saadi/stickfigure/feature_auth/presentation/forgot_password/FragmentForgotPassword.kt
@@ -5,19 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.saadi.stickfigure.R
+import com.saadi.stickfigure.databinding.FragmentForgotPasswordBinding
 
 
 class FragmentForgotPassword : Fragment() {
 
+    private lateinit var mBinding: FragmentForgotPasswordBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    ): View {
+        mBinding = FragmentForgotPasswordBinding.inflate(inflater,container,false)
+        return mBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mBinding.btnGetCode.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentForgotPassword_to_fragmentResetPassword)
+        }
+
+    }
 
 }
--- app/src/main/java/com/saadi/stickfigure/feature_auth/presentation/sign_up/FragmentSignUp.kt
+++ app/src/main/java/com/saadi/stickfigure/feature_auth/presentation/sign_up/FragmentSignUp.kt
@@ -1,23 +1,32 @@
 package com.saadi.stickfigure.feature_auth.presentation.sign_up
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.saadi.stickfigure.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.saadi.stickfigure.databinding.FragmentSignUpBinding
 
 
 class FragmentSignUp : Fragment() {
 
-
+    private lateinit var mBinding: FragmentSignUpBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    ): View {
+        mBinding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return mBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mBinding.tvAlreadyHaveAccount.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+    }
 
 }
