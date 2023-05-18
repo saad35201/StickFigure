f6f3a7c7b057a5a63be903647fec75f2b59b8d5b
saad35201 <saadasad35201@gmail.com>
2023-05-18 02:04:14 -0700

    WIP menu icon changes into backArrow for main fragments

--- app/src/main/java/com/saadi/stickfigure/feature_home_drawer/presentation/ActivityHomeBase.kt
+++ app/src/main/java/com/saadi/stickfigure/feature_home_drawer/presentation/ActivityHomeBase.kt
@@ -1,21 +1,24 @@
 package com.saadi.stickfigure.feature_home_drawer.presentation
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.*
+import android.view.Menu
+import android.view.MenuItem
+import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.graphics.drawable.DrawerArrowDrawable
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.saadi.stickfigure.R
 import com.saadi.stickfigure.databinding.ActivityHomeBinding
 import com.saadi.stickfigure.feature_auth.domain.model.sign_in.User
@@ -23,6 +26,7 @@ import com.saadi.stickfigure.feature_auth.presentation.ActivityAuthBase
 import com.saadi.stickfigure.utils.loadImageProfile
 import com.saadi.stickfigure.utils.observe
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class ActivityHomeBase : AppCompatActivity() {
@@ -32,7 +36,6 @@ class ActivityHomeBase : AppCompatActivity() {
 
     //home nav
     private lateinit var mNavController: NavController
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,77 +50,25 @@ class ActivityHomeBase : AppCompatActivity() {
         }
         setContentView(mBinding.root)
 
-        //finding home nav
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.home_nav_host_fragment) as NavHostFragment
-        mNavController = navHostFragment.navController
+        setupDrawerLayout()
 
-        mBinding.apply {
-
-            //setting toolbar and hamburger icon
-            val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
-            setSupportActionBar(toolbar)
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
-            supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
-            // Set the app bar title(default)
-            supportActionBar?.title = getString(R.string.homepage)
-
-            //setting homepage screen as default
-            mBinding.navViewDrawer.setCheckedItem(R.id.nav_home_page)
-
-            //nav click listener
-            mBinding.navViewDrawer.setNavigationItemSelectedListener {
-                when (it.itemId) {
-                    R.id.nav_my_profile -> {
-                        supportActionBar?.title = getString(R.string.my_profile)
-                        mNavController.navigate(R.id.fragmentProfile)
-                    }
-                    R.id.nav_home_page -> {
-                        supportActionBar?.title = getString(R.string.homepage)
-                        mNavController.navigate(R.id.fragmentHomePage)
-                    }
-                    R.id.nav_news_feed -> {
-                        supportActionBar?.title = getString(R.string.newsfeed)
-                    }
-                    R.id.nav_creator -> {
-                        supportActionBar?.title = getString(R.string.creator)
-                    }
-                    R.id.nav_shop -> {
-                        supportActionBar?.title = getString(R.string.shop)
-                    }
-                    R.id.nav_messages -> {
-                        supportActionBar?.title = getString(R.string.messages)
-                    }
-                    R.id.nav_my_store -> {
-                        supportActionBar?.title = getString(R.string.my_store)
-                    }
-                    R.id.nav_wallet -> {
-                        supportActionBar?.title = getString(R.string.wallet)
-                    }
-                    R.id.nav_setting -> {
-                        supportActionBar?.title = getString(R.string.settings)
-                    }
-                }
-                //closing drawer on item click
-                mBinding.drawerLayout.closeDrawers()
-                true
-            }
-
-            //cart btn click listener
-            mBinding.fabCart.setOnClickListener {
-                Toast.makeText(this@ActivityHomeBase,getString(R.string.cart), Toast.LENGTH_SHORT).show()
-            }
-
-            //logout click listener
-            mBinding.navViewDrawer.findViewById<TextView>(R.id.tv_logout).setOnClickListener {
-                mVmHomeBase.clearDataStore()
-                mBinding.drawerLayout.closeDrawers()
-                startActivity(
-                    Intent(this@ActivityHomeBase, ActivityAuthBase::class.java)
-                        .putExtra("destinationFragment", R.id.fragmentSignIn)
-                )
-                this@ActivityHomeBase.finish()
-            }
+        //cart btn click listener
+        mBinding.fabCart.setOnClickListener {
+            Toast.makeText(this@ActivityHomeBase, getString(R.string.cart), Toast.LENGTH_SHORT)
+                .show()
         }
+
+        //logout click listener
+        mBinding.navViewDrawer.findViewById<TextView>(R.id.tv_logout).setOnClickListener {
+            mVmHomeBase.clearDataStore()
+            mBinding.drawerLayout.closeDrawers()
+            startActivity(
+                Intent(this@ActivityHomeBase, ActivityAuthBase::class.java)
+                    .putExtra("destinationFragment", R.id.fragmentSignIn)
+            )
+            this@ActivityHomeBase.finish()
+        }
+
 
         //observingLiveData
         mVmHomeBase.getUser()
@@ -135,6 +86,10 @@ class ActivityHomeBase : AppCompatActivity() {
             )
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(mNavController, mBinding.drawerLayout)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return true
@@ -142,19 +97,50 @@ class ActivityHomeBase : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            android.R.id.home -> {
-                mBinding.drawerLayout.openDrawer(GravityCompat.START)
-                return true
-            }
             R.id.menu_search -> {
-                Toast.makeText(this,getString(R.string.search), Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.search), Toast.LENGTH_SHORT).show()
                 true
             }
             R.id.menu_notification -> {
-                Toast.makeText(this,getString(R.string.notification), Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.notification), Toast.LENGTH_SHORT).show()
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    private fun setupDrawerLayout() {
+        val toolbar: androidx.appcompat.widget.Toolbar = mBinding.appBar.toolbar
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            mBinding.drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        mBinding.drawerLayout.addDrawerListener(toggle)
+        toggle.isDrawerSlideAnimationEnabled = true
+        toggle.isDrawerIndicatorEnabled = true
+        toggle.syncState()
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.home_nav_host_fragment) as NavHostFragment
+        mNavController = navHostFragment.navController
+        mBinding.navViewDrawer.setupWithNavController(mNavController)
+
+        NavigationUI.setupActionBarWithNavController(this, mNavController, mBinding.drawerLayout)
+
+    }
+
+    override fun onBackPressed() {
+        if (mBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mBinding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
