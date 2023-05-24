8d6f50f6e5c8c6c639bbffb4aafa5b0a2c6ea553
saad35201 <saadasad35201@gmail.com>
2023-05-24 11:02:48 -0700

    blog wip

--- app/src/main/java/com/saadi/stickfigure/feature_blog/presentation/blogs/FragmentBlog.kt
+++ app/src/main/java/com/saadi/stickfigure/feature_blog/presentation/blogs/FragmentBlog.kt
@@ -1,4 +1,4 @@
-package com.saadi.stickfigure.feature_blog.presentation
+package com.saadi.stickfigure.feature_blog.presentation.blogs
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,15 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.saadi.stickfigure.R
+import com.saadi.stickfigure.databinding.FragmentBlogBinding
 
 class FragmentBlog : Fragment() {
+
+    //binding
+    private lateinit var mBinding: FragmentBlogBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blog, container, false)
+    ): View {
+        mBinding = FragmentBlogBinding.inflate(inflater,container,false)
+        return mBinding.root
     }
 
 }
